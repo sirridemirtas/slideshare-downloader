19b4f74c7327989512f0d0dbb1c2f26336a1d553
Sirri Demirtas <sirridemirtas@gmail.com>
2024-03-31 21:40:49 +0300

    improvements

--- app/components/Thumbnails/Thumbnails.jsx
+++ app/components/Thumbnails/Thumbnails.jsx
@@ -1,10 +1,10 @@
 "use client";
 import { useContext } from "react";
-import { AppContext, AppActions } from "../../store";
+import { AppContext } from "../../store";
 import styles from "./Thumbnails.module.css";
 
 const Thumbnails = () => {
-  const { state, dispatch } = useContext(AppContext);
+  const { state } = useContext(AppContext);
 
   return state.thumbs.length ? (
     <div className={styles.thumbs}>
